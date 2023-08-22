88e2a005697009eb28c1a07328ceb49d4f30370f
Victoria Koposova <koposova.victoria17@mail.ru>
2023-08-22 14:34:21 +0300

    Add standard component example

---
+++ src/ds_16/MyComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Test extends React.Component<any> {
+    constructor(props) {
+        super(props);
+    }
+    public render() {
+        return (
+            <div>Hello world</div>
+        );
+    }
+}
+
+export default Test;
